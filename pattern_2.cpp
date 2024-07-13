12445e6ca6eca1a3783ec5d4e596b71f589a9f36
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-13 22:06:50 +0530

    Pattern 2.

---
+++ pattern_2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cin>>r;
+    cin>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(i==0 || i==r-1 || j==0 || j==c-1)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
