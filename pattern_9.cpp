5db42257bd0d3abe57ade1f93e060cbe628fe02f
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-13 22:08:14 +0530

    Pattern 9.

---
+++ pattern_9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cout<<"Enter Row : ";
+    cin>>r;
+    cout<<"Enter Col : ";
+    cin>>c;
+    for(int i=r;i>0;i--)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
