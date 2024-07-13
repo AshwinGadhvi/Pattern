79118ed5116662f712205f9bbbd58458fccb3b6f
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-13 22:07:07 +0530

    Pattern 4.

---
+++ pattern_4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cin>>r;
+    cin>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<i)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
