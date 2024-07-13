93c05fca98e16cdb58f8db3d0b5f3e152af149e0
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-13 22:08:36 +0530

    Pattern 11.

---
+++ pattern_11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cout<<"Enter Row : ";
+    cin>>r;
+    cout<<"Enter Col : ";
+    cin>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+}
