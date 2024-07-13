e0c128b30d77f916bde3d3a2c2d15b35059a9756
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-13 22:08:00 +0530

    Pattern 8.

---
+++ pattern_8.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cout<<"Enter Row : ";
+    cin>>r;
+    cout<<"Enter Col : ";
+    cin>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<i)
+            {
+                cout<<"  ";
+            }
+            else
+            {
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+}
